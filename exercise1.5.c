7f6d4f0d2fdf681d6194ca7be57580817ded9e32
nhuquynh14 <81420604+nhuquynh14@users.noreply.github.com>
2021-03-28 20:10:38 +0700

    Create exercise1.5.c

---
+++ exercise1.5.c
@@ -0,0 +1,30 @@
+//Tran Thi Nhu Quynh 20205122
+
+#include<stdio.h>
+
+int main(){
+	int a;
+	int s = 100;
+	printf("You have $100. Let's choose some things\n1. Vietnam CD($30)\n2. America CD($35)\n3. Korea CD($30)\n4. China CD($35)\n");
+	scanf("%d", &a);
+	
+		if( a== 1){
+			
+			printf("You have one Vietnam CD");
+		}
+		if( a== 2){
+			
+			printf("You have one America CD");
+		}
+		if( a== 3){
+			
+			printf("You have one Korea CD");
+		}
+		if( a== 4){
+			
+			printf("You have one China CD");
+		}
+		
+		
+	
+}
